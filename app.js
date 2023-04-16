ddcd2f635913c1c61942c205b5e296712f1191a7
anouaraissani <aaissani571@gmail.com>
2023-04-16 14:27:13 +0200

    create first route and test db connection

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const { connect, close } = require('./db/connect')
+const routes = require('./routes/test')
 
+const app = express()
+
+app.use(express.json())
+app.use('/api', routes)
+
+connect()
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
+
+// Gracefully shutdown the server on interrupt signals
+process.on('SIGINT', async () => {
+  console.log('Shutting down server gracefully')
+  await close()
+  process.exit(0)
+})
