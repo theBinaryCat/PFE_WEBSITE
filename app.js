d71e66a1c6482a7ef3873689d606af44f30037ee
anouaraissani <aaissani571@gmail.com>
2023-05-01 01:22:53 +0200

    initialize our routes

--- app.js
+++ app.js
@@ -8,9 +8,30 @@ app.use(express.json())
 app.use('/api', routes)
 
 connect()
+//set the view engine for our application to EJS (rendering dynamic HTML to client requests)
+app.set('view-engine', 'ejs')
+app.get('/', (req, res) => {
+  res.render('index.ejs', { name: 'test' })
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+  
+})
 
 // Start the server
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
