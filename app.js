bf983c737804801979978cf150bc9df26b875e03
anouaraissani <aaissani571@gmail.com>
2023-05-01 14:35:17 +0200

    user authentication

--- app.js
+++ app.js
@@ -1,32 +1,89 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
-const { connect, close, getConnection } = require('./db/connect')
 const routes = require('./routes/test')
+const { connect, close, getConnection } = require('./db/connect')
 const app = express()
 //hash and compare passwords
 const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
 
-
-app.use(express.json())
-app.use('/api', routes)
 
 connect()
+
+const initialize = require('./passport-config')
+initialize(
+  passport,
+  async (email) => {
+    try {
+
+      const connection = await getConnection()
+      const result = await connection.execute(
+        'SELECT * FROM users WHERE email = :email',
+        {email}
+      )
+      await close();
+      if (result.rows.length > 0) {
+        const user = result.rows[0]
+        console.log('test get user by email',user[3])
+        return user
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  
+  async (id) => {
+    try {
+      const connection = await getConnection()
+      const result = await connection.execute(
+        'SELECT * FROM users WHERE TO_NUMBER(id) = :id',
+        {id}
+      )
+      await close();
+      if (result.rows.length > 0) {
+        const user = result.rows[0]
+        console.log('test get user by id',user)
+        return user
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+)
+
 //set the view engine for our application to EJS (rendering dynamic HTML to client requests)
 app.set('view-engine', 'ejs')
-
 //Take data from the forms and build access to them inside our request variable in post method
 app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', { name: 'test' })
+  console.log(req.user)
+  res.render('index.ejs', { name: req.user.name })
 })
 
 app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
-
-})
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 app.get('/register', (req, res) => {
   res.render('register.ejs')
@@ -56,7 +113,7 @@ app.post('/register', async (req, res) => {
     res.redirect('/login')
   } catch (e){
     console.error(e)
-    //if the registration is failes, redirect to registration page
+    //if the registration is failed, redirect to registration page
     res.redirect('/register')
   }
 })
---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+//initialize our server   
+function initialize(passport,getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
+        const user = await getUserByEmail(email)
+        console.log('the user is',user)
+        if (user == null) {
+            return done(null, false, { message: 'No user with that email' })
+        }
+        try {
+            if (await bcrypt.compare(password, user[3])) {
+                return done(null, user)
+        } else {
+            return done(null, false, { message: 'Password incorrect' })
+        }
+        } catch (e) {
+            return done(e)
+        }
+    }
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user[0]))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
