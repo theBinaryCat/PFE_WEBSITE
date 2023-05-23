6e4b0958d091e18b55593073d6059a4ddfd5e018
anouaraissani <aaissani571@gmail.com>
2023-05-23 22:04:41 +0200

    fetch data from the database

---
+++ queries.js
@@ -0,0 +1,25 @@
+//dashboard: chiffre d'affaire
+const queryCA1 = `
+    SELECT d.annee, SUM(CA) 
+    FROM FAIT_PRODUCTION f, DIM_DATE d
+    WHERE f.date_key = d.date_key
+    GROUP BY d.annee
+    ORDER BY d.annee
+`
+
+const queryCA2 = `
+    SELECT CAT.libebran, SUM(CA)
+    FROM FAIT_PRODUCTION prod, DIM_CATEGORIE cat
+    WHERE cat.categorie_key = prod.categorie_key
+    GROUP BY cat.libebran
+    ORDER BY cat.libebran
+`
+
+const queryCA3 = `SELECT d.mois, SUM(CA) 
+FROM DATAWH.FAIT_PRODUCTION f, DATAWH.DIM_DATE d
+WHERE f.date_key = d.date_key
+AND d.annee = 2017
+GROUP BY d.mois
+ORDER BY d.mois`
+
+module.exports={queryCA1, queryCA2, queryCA3}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {dashboard} = require('../controllers/dashboard')
+const {dashboard, fetchDataCA} = require('../controllers/dashboard')
 const {checkAuthenticated} = require('../middlewares/authentification')
 
 
 router.get('/', checkAuthenticated, dashboard)
+router.get('/api/data', checkAuthenticated, fetchDataCA)
+router.get('/chiffre-daffaire', checkAuthenticated, (req, res) => {
+    res.render('dashboard.ejs')})
 
 module.exports = router
