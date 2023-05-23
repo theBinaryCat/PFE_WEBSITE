6c46a4cb0d860caa09f46b81d476b7ac8ef357a2
anouaraissani <aaissani571@gmail.com>
2023-05-23 22:15:50 +0200

    visualize first dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {dashboard, fetchDataCA} = require('../controllers/dashboard')
 const {checkAuthenticated} = require('../middlewares/authentification')
-
+//use static files(.css, .html, ...)
+router.use(express.static('./views'))
 
 router.get('/', checkAuthenticated, dashboard)
 router.get('/api/data', checkAuthenticated, fetchDataCA)
 router.get('/chiffre-daffaire', checkAuthenticated, (req, res) => {
-    res.render('dashboard.ejs')})
+    res.render('dashboard_CA.ejs')})
 
 module.exports = router
